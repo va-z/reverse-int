179531a8298a8d73fe5ee1e8c9f3117307d7c00c
va-z <vasilizadorojnyuk@gmail.com>
2020-09-20 21:44:48 +0500

    feat: solve index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) n *= -1;
+    let result = 0;
+
+    while (n !== 0) {
+        result *= 10;
+        result += n % 10;
+        n = (n / 10) | 0;
+    }
+    return result;
+};
